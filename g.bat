099459d84e3bc61c4e965f6116ad5b4af5ba8e66
WuJun <1811716974@qq.com>
2022-06-24 18:10:04 +0800

    WuJun

--- g.bat
+++ g.bat
@@ -3,7 +3,7 @@ git status
 :: add all changing
 git add -A
 :: local commit
-git commit -m "工作电脑提交"
+git commit -m "WuJun"
 :: show git pull
 git pull origin master --no-edit
 :: push to remote repository
